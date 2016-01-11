ce367fa5275e32b8657b20a415bcbba4c72af3f1
Daniel Klamt <eleon@ele0n.de>
2016-01-11 22:34:40 +0100

    initial commir

---
+++ BottleSimulator.py
@@ -0,0 +1,31 @@
+import pygame
+import time
+
+class BottleSimulator:
+
+    def __init__(self,x,y,scale):
+        self.array = (x*y - (y-y%2)/2)*[2*[0]]
+        self.x = x
+        self.y = y
+        self.scale = scale
+        self.screen = pygame.display.set_mode((x*20*scale,y*20*scale))
+        pygame.display.set_caption("FrozenBottle")
+        pygame.init()
+        self.screen.fill((255,255,255))
+
+        for i in range(y):
+            if(i%2 == 0):
+                for j in range(x):
+                    self.array[len(self.array)-1-(j+i*x-(i+i%2)/2)] = (10*scale+j*20*scale,10*scale+i*scale*17)
+                    pygame.draw.circle(self.screen,(0,0,0),(10*scale+j*20*scale,10*scale+i*17*scale),10*scale,2*scale)
+            else:
+                for j in range(x-1):
+                    self.array[len(self.array)-1-((x-1-j)+x*i-(i+i%2)/2)] = (20*scale+j*20*scale,10*scale+i*17*scale)
+                    pygame.draw.circle(self.screen,(0,0,0),(20*scale+j*20*scale,10*scale+i*17*scale),10*scale,2*scale)
+
+        pygame.display.update()
+
+    def send(self,i,r,g,b):
+        pygame.draw.circle(self.screen,(r,g,b),(self.array[i][0],self.array[i][1]),8*self.scale,0)
+        pygame.display.update()
+
