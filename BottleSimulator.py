6727fe7c8ceffa18ff0fcd94e7d4645d66cca986
Daniel Klamt <eleon@ele0n.de>
2016-01-14 23:59:43 +0100

    simulator using coordinate approach

--- BottleSimulator.py
+++ BottleSimulator.py
@@ -1,44 +1,68 @@
 import pygame
 
 
-class BottleSimulator:
-    def __init__(self, x, y, scale=4):
-        self.array = (x*y-(y-y % 2)/2)*[2*[0]]
-        self.x = x
-        self.y = y
-        self.scale = scale
-        self.screen = pygame.display.set_mode((x*20*scale, y*20*scale))
+class BottleSimulator(object):
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+
+    def __init__(self, cols, rows, resolution=(800, 600)):
+        self.cols = cols
+        self.rows = rows
+
+        self.screen = pygame.display.set_mode(resolution)
+
+        # radius is calculated relative to rows/columns
+        self.radius = min((self.screen.get_width() // self.cols // 2,
+                          self.screen.get_height() // self.rows // 2))
+
+        # odd lines are missing 1 bottle
+        odd = (rows - rows % 2) / 2
+        self.bottles = [self.black] * ((rows*cols)-odd)
+
         pygame.display.set_caption("FrozenBottle")
         pygame.init()
-        self.screen.fill((255, 255, 255))
 
-        for i in range(y):
-            if i % 2 == 0:
-                for j in range(x):
-                    idx = len(self.array)-1-(j+i*x-(i+i % 2)/2)
-                    self.array[idx] = 10*scale+j*20*scale, 10*scale+i*scale*17
-                    pygame.draw.circle(self.screen, (0, 0, 0),
-                                       (10*scale+j*20*scale,
-                                       10*scale+i*17*scale), 10*scale, 0)
-                    pygame.draw.circle(self.screen, (255, 255, 255),
-                                       (10*scale+j*20*scale,
-                                       10*scale+i*17*scale), 8*scale, 0)
-            else:
-                for j in range(x-1):
-                    idx = len(self.array)-1-((x-1-j)+x*i-(i+i % 2)/2)
-                    self.array[idx] = 20*scale+j*20*scale, 10*scale+i*17*scale
-                    pygame.draw.circle(self.screen, (0, 0, 0),
-                                       (20*scale+j*20*scale,
-                                       10*scale+i*17*scale),
-                                       10*scale, 0)
-                    pygame.draw.circle(self.screen, (255, 255, 255),
-                                       (20*scale+j*20*scale,
-                                       10*scale+i*17*scale),
-                                       8*scale, 0)
+        self.screen.fill(self.white)
+        self.draw_bottles()
 
-        pygame.display.update()
+    def i_to_xy(self, i):
+        # compensate for odd lines missing 1 bottle
+        for k in range(1, i, 2):
+            if i >= self.cols-1+self.cols*k:
+                i += 1
 
-    def send(self, i, r, g, b):
-        pygame.draw.circle(self.screen, (r, g, b), (self.array[i][0],
-                           self.array[i][1]), 8*self.scale, 0)
+        x = i % self.cols
+        y = i // self.cols
+
+        return x, y
+
+    def draw_bottles(self):
+        for i, color in enumerate(self.bottles):
+            self.draw_bottle_i(i, color)
+
+    def draw_bottle_i(self, i, color, border=5):
+        x, y = self.i_to_xy(i)
+        # print("Drawing %d to (%d,%d)" % (i, x, y))
+        self.draw_bottle_xy(x, y, color, border)
+
+    def draw_bottle_xy(self, x, y, color, border=5, virtual_exc=True):
+        pos_x = self.radius + x*self.radius*2
+        # bottles should "lay" on each other
+        pos_y = self.radius + y*self.radius*2 - y*15
+
+        # odd rows have one bottle less than even rows
+        if y % 2 != 0:
+            if x == self.cols-1:
+                if virtual_exc:
+                    raise Exception('No such bottle: (%d,%d)' % (x, y))
+                return
+            pos_x += self.radius
+
+        # draw border
+        pygame.draw.circle(self.screen, self.black, (pos_x, pos_y),
+                           self.radius)
+        # draw bottle bottom
+        pygame.draw.circle(self.screen, color, (pos_x, pos_y),
+                           self.radius-border*2)
+
         pygame.display.update()
--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 from BottleSimulator import BottleSimulator
 import time
 
-c = BottleSimulator(7,5,4)
+c = BottleSimulator(7, 5)
+
 for i in range(11):
-    c.send(i*3,255,0,0)
-    c.send(i*3+1,0,255,0)
-    c.send(i*3+2,0,0,255)
+    c.draw_bottle_i(i*3, (255, 0, 0))
+    c.draw_bottle_i(i*3+1, (0, 255, 0))
+    c.draw_bottle_i(i*3+2, (0, 0, 255))
+
 time.sleep(100)
